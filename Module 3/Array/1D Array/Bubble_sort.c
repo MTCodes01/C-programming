9398c5bbcd085c69af9cb693485c3e1a805ad380
MTCodes01 <koshi9866@gmail.com>
2024-06-17 14:09:22 +0530

    minor change

--- Module 3/Array/1D Array/Bubble_sort.c
+++ Module 3/Array/1D Array/Bubble_sort.c
@@ -6,7 +6,7 @@ int main() {
     int A[20];
     printf("Enter the elements: ");
     for (int i=0; i<n; i++) {
-        scanf("%d ", &A[i]);
+        scanf("%d", &A[i]);
     }
     for (int i=0; i<n-1; i++) {
         for (int j=0; j<n-i-1; j++) {
