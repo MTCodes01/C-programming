ab6d791be1c4684eedd4e07f18522f74ff363323
MTCodes01 <koshi9866@gmail.com>
2024-06-17 12:14:13 +0530

    bubble sort

---
+++ Module 3/Array/1D Array/Bubble_sort.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+    int n = 1;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    int A[20];
+    printf("Enter the elements: ");
+    for (int i=0; i<n; i++) {
+        scanf("%d ", &A[i]);
+    }
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<n-i-1; j++) {
+            if (A[j]>A[j+1]) {
+                A[j] += A[j+1];
+                A[j+1] = A[j] - A[j+1];
+                A[j] -= A[j+1];
+            }
+        }
+    }
+    printf("Sorted List: ");
+    for (int i=0; i<n; i++) {
+        printf("%d ", A[i]);
+    }
+}
